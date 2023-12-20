542f2a16e262a68ea99a1333e621e60b79c91c4c
Michael Akinyemi <mikedamilare@gmail.com>
2023-12-20 16:18:31 +0000

    restructure app to .net 6 program file standard structure

---
+++ Greggs.Products.Api/Extenstions/ProgramExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Greggs.Products.Api.Extenstions
+{
+    public static class ProgramExtensions
+    {
+        public static void AddSerilogConfig(this WebApplicationBuilder builder)
+        {
+            builder.Logging.ClearProviders();
+            builder.Host.UseSerilog((context, services, configuration) =>
+            {
+                configuration.ReadFrom.Configuration(context.Configuration)
+                    .WriteTo.Console();
+            });
+        }
+
+        public static void AddCorsConfig(this IApplicationBuilder app)
+        {
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .WithMethods("GET") // <-- Limit API to GET requests only
+                    .WithHeaders("Content-Type", "accept");
+            });
+        }
+    }
+}
--- Greggs.Products.Api/Program.cs
+++ Greggs.Products.Api/Program.cs
@@ -1,16 +1,57 @@
+using Greggs.Products.Api.DataAccess;
+using Greggs.Products.Api.Extenstions;
+using Greggs.Products.Api.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
-namespace Greggs.Products.Api;
-
-public class Program
+try
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    var builder = WebApplication.CreateBuilder(args);
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    // Add Serilog config to the builder
+    builder.AddSerilogConfig(); // <--- This is the extension method
+
+    // Add services to the container.
+    var services = builder.Services;
+
+    services.AddSingleton<IDataAccess<Product>, ProductAccess>();
+    services.AddControllers();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new() { Title = "Greggs.Products.Api", Version = "v1" });
+    });
+
+
+    // WebApplicationBuilder app
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Greggs Products Api V1");
+        c.DocumentTitle = "Greggs Products API";
+    });
+
+    #region Middlewares
+    // Middlewares here
+    
+    #endregion
+    
+    app.UseHttpsRedirection();
+    app.AddCorsConfig();
+    app.UseRouting();
+    app.UseAuthorization();
+    app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+
+    // Run the application.
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
 }
